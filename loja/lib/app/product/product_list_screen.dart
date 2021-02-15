42216ffdddfa69061e4d4883c001d06627ba4808
WillianyV <williany_thalita@hotmail.com>
2021-02-15 17:30:07 -0300

    tela de lista de produtos

--- loja/lib/app/product/product_list_screen.dart
+++ loja/lib/app/product/product_list_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:loja/components/drawer_loja.dart';
 import 'package:loja/routes/AppRoutes.dart';
 import 'package:loja/shared/Constants.dart';
+import 'package:loja/app/product/product_model.dart';
+import 'package:loja/shared/repository_shared.dart';
 
 class Product_List extends StatefulWidget {
   @override
@@ -10,9 +12,13 @@ class Product_List extends StatefulWidget {
 }
 
 class _Product_ListState extends State<Product_List> {
-  //ArrayList de produtos
-  void initState() {
+  List<Product> products;
+
+  RepositoryShared repositoryShared = new RepositoryShared();
+
+  Future<void> initState() async {
     super.initState();
+    this.products = await repositoryShared.findAllProducts();
   }
 
   @override
@@ -34,5 +40,58 @@ class _Product_ListState extends State<Product_List> {
   }
 
   Widget _body() {
+    return SingleChildScrollView(
+        child: ListView.builder(
+            itemCount: products.length,
+            itemBuilder: (ctx, i) {
+              Product product = products[i];
+              return ListTile(
+                leading: Image.network(product.imagem_url),
+                title: Text(product.name),
+                subtitle: Text(product.price.toStringAsFixed(2)),
+                trailing: Container(
+                    width: 100,
+                    child: Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.edit),
+                          color: Colors.orange,
+                          onPressed: () {
+                            // Navigator.of(context).pushNamed();
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.delete),
+                          color: Colors.red,
+                          onPressed: () {
+                            showDialog(
+                              context: context,
+                              builder: (ctx) => AlertDialog(
+                                title: Text('Excluir Produto'),
+                                content: Text('Tem certeza?'),
+                                actions: [
+                                  FlatButton(
+                                    child: Text('Não'),
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(false),
+                                  ),
+                                  FlatButton(
+                                    child: Text('Sim'),
+                                    onPressed: () =>
+                                        Navigator.of(context).pop(true),
+                                  ),
+                                ],
+                              ),
+                            ).then((confimed) {
+                              if (confimed) {
+                                //remover
+                              }
+                            });
+                          },
+                        ),
+                      ],
+                    )),
+              );
+            }));
   }
 }
--- loja/lib/shared/urls.dart
+++ loja/lib/shared/urls.dart
@@ -1,13 +1,13 @@
-const String dominio = "192.168.100.8";
+const String dominio = "192.168.100.37";
 
 const String keyApiImg = "3ce2b86738aec37f2a04c91cf31412ce";
 
 const api = "http://${dominio}:8080/api/";
 
 const url_login = "${api}loja/";
-//Products
 const url_findAllProducts = "${api}produtos/";
-const url_createProduct = "${api}produto";
 
 const url_uploadImg =
     "https://api.imgbb.com/1/upload?expiration=600&key=${keyApiImg}";
+
+const url_createProduct = "${api}produto";
